9b6f54b8c7bb42cfba6926a51003def905a29c81
lucas11776 <lucas11776>
2026-04-23 13:49:21 +0200

    Updated: Change sqlx to sqlx-flyer custom package.

--- build.rs
+++ build.rs
@@ -1,14 +1,14 @@
-use std::fs;
+// use std::fs;
 
-use git2::{Error, Repository};
+// use git2::{Error, Repository};
 
-const REPOSITORY: &str = "https://github.com/lucas11776-golang/sqlx.git";
-const PATH: &str = "./sqlx";
+// const REPOSITORY: &str = "https://github.com/lucas11776-golang/sqlx.git";
+// const PATH: &str = "./sqlx";
 
 fn main() -> Result<(), git2::Error> {
-    if let Err(_) = fs::read_dir(PATH) {
-        return Repository::clone(REPOSITORY, PATH).map_err(|err| Error::from_str(&err.to_string()))
-            .map(|_| ());
-    }
+    // if let Err(_) = fs::read_dir(PATH) {
+    //     return Repository::clone(REPOSITORY, PATH).map_err(|err| Error::from_str(&err.to_string()))
+    //         .map(|_| ());
+    // }
     Ok(())
 }

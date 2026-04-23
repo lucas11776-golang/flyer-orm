556028d26c99db377ff56eb04675b6807747adc0
lucas11776 <lucas11776>
2026-04-23 10:28:06 +0200

    Added: build.rs

---
+++ build.rs
@@ -0,0 +1,14 @@
+use std::fs;
+
+use git2::{Error, Repository};
+
+const REPOSITORY: &str = "https://github.com/lucas11776-golang/sqlx.git";
+const PATH: &str = "./sqlx";
+
+fn main() -> Result<(), git2::Error> {
+    if let Err(_) = fs::read_dir(PATH) {
+        return Repository::clone(REPOSITORY, PATH).map_err(|err| Error::from_str(&err.to_string()))
+            .map(|_| ());
+    }
+    Ok(())
+}
